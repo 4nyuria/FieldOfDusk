5d50dff0ac39103a8204e94b516facadf6a31400
Anyuria <giuganmo@gmail.com>
2025-11-07 17:04:14 +0000

    actualizar .java

---
+++ Main/tile/TileManager.java
@@ -0,0 +1,74 @@
+package Main.tile;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import Main.GamePanel;
+
+public class TileManager {
+    GamePanel gp;
+    tile[] tile;
+    
+    public TileManager(GamePanel gp) {
+        this.gp = gp;
+
+        tile = new tile[10]; //este número depende de la cantidad de tiles que tengamos
+      
+        getTileImage();
+
+         // Inicializar el mapa con el tile base
+         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
+          for (int col = 0; col < gp.maxWorldCol; col++) {
+                for (int row = 0; row < gp.maxWorldRow; row++) {
+                 mapTileNum[col][row] = 0; // todo pasto por ahora
+             }
+            }
+    }
+    public void getTileImage() { //cargar las imágenes de los tiles
+    try {
+     
+    
+        tile[0] = new tile();
+        tile[0].image = ImageIO.read(getClass().getResourceAsStream("/Res/tiles/001.png"));
+        if (tile[0].image == null) System.out.println("⚠️ No se encontró 000.png");
+        tile[1] = new tile();
+        tile[1].image = ImageIO.read(getClass().getResourceAsStream("/Res/tiles/002.png"));
+        if (tile[0].image == null) System.out.println("⚠️ No se encontró 001.png");
+        tile[2] = new tile();
+        tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Res/tiles/003.png")); 
+        if (tile[0].image == null) System.out.println("⚠️ No se encontró 002.png");
+        tile[1].collision = true;
+
+        //Cargar más tiles según sea necesario
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    public void draw(Graphics2D g2) {
+        int worldCol = 0;
+        int worldRow = 0;
+        int x =0;//lo uso porque sale en el video xdd
+        int y =0;// lo mismo con este
+        
+        while (worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
+            int tileNum = mapTileNum[worldCol][worldRow];
+            
+            int worldX = worldCol * gp.tileSize;
+            int worldY = worldRow * gp.tileSize;
+          
+            g2.drawImage(tile[tileNum].image, worldX, worldY, gp.tileSize, gp.tileSize, null);
+            
+            worldCol++;
+            
+            if (worldCol == gp.maxWorldCol) {
+                worldCol = 0;
+                x=0;
+                worldRow++;
+                y += gp.tileSize;
+            }
+        }
+    }
+
+}
+}
+
---
+++ Main/tile/tile.java
@@ -0,0 +1,8 @@
+package Main.tile;
+
+import java.awt.image.BufferedImage;
+
+public class tile {
+    Public BufferedImage image;
+    public boolean collision = false;
+}
